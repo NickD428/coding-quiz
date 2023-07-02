37ea069dd178039b0dffde68c335040e981210f5
Nick <chargerfan428@aol.com>
2023-07-02 14:23:26 -0700

    added questions for the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      title: 'Commonly used languages in web development DO NOT include:',
+      choices: ['javascript', 'html', 'C++', 'css'],
+      answer: 'C++',
+    },
+    {
+      title: 'HTML is used to:',
+      choices: ['build barebones websites', 'style websites', 'add website functionality', 'play games'],
+      answer: 'build barebones websites',
+    },
+    {
+      title: 'Arrays are used for:',
+      choices: [
+        'storing data',
+        'storing other arrays',
+        'storing strings',
+        'all of the above',
+      ],
+      answer: 'all of the above',
+    },
+    {
+      title:
+        'The purpose of a string is to:',
+      choices: ['show text', 'do equations', 'nothing', 'talk to the computer'],
+      answer: 'show text',
+    },
+    {
+      title:
+        'A good way to mess around with code without breaking it is to use:',
+      choices: ['booleans', 'chrome dev tools', 'vs code', 'copy and pasting'],
+      answer: 'chrome dev tools',
+    },
+  ];
+  
