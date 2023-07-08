356501293a343904e5a4ebf6d8ae8e8a5566e106
Nick <chargerfan428@aol.com>
2023-07-08 12:23:29 -0700

    added start and submit buttons and functions to start the quiz and update the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,27 @@ var currentQuestionIndex = 0;
 var time = 0;
 var timerInterval;
 
+startButton.addEventListener("click", startQuiz);
+
+submitButton.addEventListener("click", saveScore);
+
+function startQuiz() {
+  document.getElementById("start-screen").classList.add("hide");
+  document.getElementById("questions").classList.remove("hide");
+
+  time = 60;
+  timeElement.textContent = time;
+  timerInterval = setInterval(updateTime, 1000);
+
+  showQuestion();
+}
+
+function updateTime() {
+  time--;
+  timeElement.textContent = time;
+
+  if (time <= 0) {
+    endQuiz();
+  }
+}
+
